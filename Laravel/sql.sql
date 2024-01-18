7ec90e43900f34dbe2f0377fe19270ed28debe1d
Khang nguyen <ronglun412@gmail.com>
2024-01-18 13:07:09 +0700

    content (no edit): 1:07 - 18/1/24

--- Laravel/sql.sql
+++ Laravel/sql.sql
@@ -6,3 +6,14 @@ create table test(
 select * from test;
 alter table test drop column id;
 drop table test;
+
+
+
+
+-- truy xuất db mẫu
+select * from acc_transaction;
+select * from account;
+
+select  acc_transaction.ACCOUNT_ID, account.ACCOUNT_ID, AVAIL_BALANCE, LAST_ACTIVITY_DATE, PENDING_BALANCE, AMOUNT from account 
+join acc_transaction 
+on account.ACCOUNT_ID = acc_transaction.ACCOUNT_ID

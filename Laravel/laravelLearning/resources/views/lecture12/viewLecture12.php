326ae8fb8b83778b76003d62a50ccc471ad342a2
khangnguyen412 <119936631+khangnguyen412@users.noreply.github.com>
2024-03-29 13:10:57 +0700

    content (no edit): 01:00 - 29/03/2024

--- Laravel/laravelLearning/resources/views/lecture12/viewLecture12.php
+++ Laravel/laravelLearning/resources/views/lecture12/viewLecture12.php
@@ -1,1 +1,18 @@
-<pre>Kết Quả: <?php echo json_encode($data, JSON_PRETTY_PRINT); ?></pre>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <pre>Kết Quả: <?php echo json_encode($data, JSON_PRETTY_PRINT); ?></pre>
+    <script>
+        setTimeout(function() {
+            window.location.href = "/getDataModel12"
+        }, 2000); // 2 second
+    </script>
+</body>
+
+</html>
